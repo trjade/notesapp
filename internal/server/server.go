050969620ad73f731078415a9dc9e39eaf0c5b58
Trupti Jade <trupti.jade@neosoftmail.com>
2025-07-22 16:38:28 +0530

    added ip

--- internal/server/server.go
+++ internal/server/server.go
@@ -26,7 +26,7 @@ func Start() {
 
 
 	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
